e98a01865d15797712d3d5e4708a219e625141f4
Matheus Julidori <matheus_julidori@yahoo.com.br>
2022-03-10 18:33:18 -0300

    Ler aulas no Notion

--- Aula3/S202-mongo-query/main.py
+++ Aula3/S202-mongo-query/main.py
@@ -2,5 +2,14 @@ from db.database import Database
 from helper.WriteAJson import writeAJson
 
 db = Database(database="pokedex", collection="pokemons")
-db.resetDatabase()
+#db.resetDatabase()
+
+tipos = ['Grass','Poison']
+
+pokemons = db.collection.find(
+    db.executeQuery({'type':{'$in':tipos},"next_evolution":{"exists":True}}),
+    {'type':1}
+)
+
+writeAJson(pokemons,"pokemons")
 
