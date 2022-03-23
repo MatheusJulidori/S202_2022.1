93836cf58b148aa24e6dd85f08572a9f2a6a2de9
Matheus Julidori <matheus_julidori@yahoo.com.br>
2022-03-23 10:41:15 -0300

    Exercicios

--- Lab/Aula2/main.py
+++ Lab/Aula2/main.py
@@ -4,3 +4,35 @@ from helper.WriteAJson import writeAJson
 db = Database(database="pokedex", collection="pokemons")
 db.resetDatabase()
 
+#Exercicios
+
+#1 Fracos contra fogo e gelo com evolução
+weaknesses = ["Fire", "Ice"]
+pokemons = db.executeQuery({"weaknesses": {"$all": weaknesses},"next_evolution": {"$exists": True}})
+
+writeAJson(pokemons,"Query1")
+
+
+#2 Psiquicos que tem duas fraquesas
+typeP = ["Psychic"]
+pokemons = db.executeQuery({"weaknesses": {"$size": 2},"type": {"$all": typeP}})
+
+writeAJson(pokemons,"Query2")
+
+# Fogos e gelo com spawn chance menor que 0.2
+types = ["Fire", "Ice"]
+pokemons = db.executeQuery({"type": {"$all": types},"spawn_chance": {"$lt":0.2}})
+
+writeAJson(pokemons,"Query3")
+
+#Pokemons com duas fraquezas
+pokemons = db.executeQuery({"weaknesses": {"$size": 2}})
+
+writeAJson(pokemons,"Query4")
+
+#Fracos contra grama e veneno ou com chance de spawn maior que 0.5
+weaknesses = ["Grass", "Poison"]
+pokemons = db.executeQuery({"$or": [{"spawn_chance": {"$gt":0.5}},{"weaknesses": {"$all":weaknesses}}]})
+
+writeAJson(pokemons,"Query5")
+
