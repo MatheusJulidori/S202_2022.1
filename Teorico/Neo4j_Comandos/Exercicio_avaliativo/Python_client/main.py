fff2e3a282dea8e039d4e297738cd742e9da0c43
Matheus Julidori <m.henrique@ges.inatel.br>
2022-05-30 13:52:14 -0300

    Projeto

--- Teorico/Neo4j_Comandos/Exercicio_avaliativo/Python_client/main.py
+++ Teorico/Neo4j_Comandos/Exercicio_avaliativo/Python_client/main.py
@@ -3,15 +3,12 @@ from db.database import Graph
 
 
 db = Graph("bolt://44.201.19.132:7687", "neo4j", "damping-bomb-debit")
-data = db.execute_query("match (n) return n")
-write_a_json(data, "all_data")
-
 
 def quemECrianca():
     return db.execute_query("match (n:Criança) return n")
     
 def desdeQuandoECasado(nomeP):
-    return db.execute_query("match (n:Person)-[cc:CASADO_COM]->(n1:Person) return n1.name,cc.desde,n.name")
+    return db.execute_query("match (n:Person)-[cc:CASADO_COM]->(n1:Person) WHERE n.name=$nomeP return n1.name,cc.desde,n.name",{'nomeP':nomeP})
 
 def qualAIdadeDaPessoa(nomeP):
     return db.execute_query("Match (p:Person) WHERE p.name = $nomeP RETURN p.age",{'nomeP':nomeP})
